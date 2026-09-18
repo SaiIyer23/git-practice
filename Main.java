16169904eb09f79df91f5ad74ba6b4b6f9b83f55
Iyer <saiiyer69@gmail.com>
2026-09-18 12:48:13 +0530

    Add line for revert practice

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("I am learning Git for SDE");
+        System.out.println("Practicing Git Staging");
+        System.out.println("This commit will be reverted");
     }
 }
